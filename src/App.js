23f1687c6b4e308da253ac3850baa2304625c65c
Md Rokan Uddin <rokanuddinroman@gmail.com>
2022-04-05 14:14:56 +0600

    Error Page Updated

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Reviews from './components/Reviews/Reviews';
 import Dashboard from './components/Dashboard/Dashboard';
 import Blogs from './components/Blogs/Blogs';
 import About from './components/About/About';
+import NotFound from './components/NotFound/NotFound';
 function App() {
   return (
     <div className="App">
@@ -17,6 +18,7 @@ function App() {
         <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='*' element={<NotFound />}></Route>
       </Routes>
     </div>
   );
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NotFound.css'
+const NotFound = () => {
+    return (
+        <div className='container errorpage-container'>
+            <img src="https://cdn-icons-png.flaticon.com/512/3399/3399533.png" alt="" />
+            <h2>Page Not Found</h2>
+        </div>
+    );
+};
+
+export default NotFound;
