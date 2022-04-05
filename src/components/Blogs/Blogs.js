71a599f1b742486b6743a1930f6b1be00a397959
Md Rokan Uddin <rokanuddinroman@gmail.com>
2022-04-05 13:57:21 +0600

    Blogs Page Updated

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import './Blogs.css'
 const Blogs = () => {
     return (
-        <div>
-            <h1>Blogs Page</h1>
+        <div className='container'>
+            <h2>Blogs Page</h2>
+            <div className="blogs__container">
+                <div>
+                    <h2>What is Semanctic Tag?</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat nostrum illo quia odio est, sapiente saepe. Nesciunt impedit incidunt praesentium. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat nostrum illo quia odio est, sapiente saepe. Nesciunt impedit incidunt praesentium.</p>
+                </div>
+                <div>
+                    <h2>What is Semanctic Tag?</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat nostrum illo quia odio est, sapiente saepe. Nesciunt impedit incidunt praesentium. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat nostrum illo quia odio est, sapiente saepe. Nesciunt impedit incidunt praesentium.</p>
+                </div>
+            </div>
         </div>
     );
 };
